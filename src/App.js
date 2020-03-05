efd7c8c4b6f4a351cdbad7cf4740ebe919fc5ee8
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-03-05 16:42:51 +0100

    restructuring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import gs from './App.module.css';
-import Layout from './components/Layout/Layout'; 
+import Layout from './containers/Layout/Layout'; 
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 
