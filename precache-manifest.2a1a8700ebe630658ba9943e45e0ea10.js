05c61645e1e84da971cf7db9d318551e4fad1e74
Dmitriy Bondarenko <demianbondarenko@gmail.com>
2020-04-20 20:43:59 +0200

    Updates

---
+++ precache-manifest.2a1a8700ebe630658ba9943e45e0ea10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf28d900833a949879eb35cb24499982",
+    "url": "/github.io/burger-constructor/index.html"
+  },
+  {
+    "revision": "a066cf9985e6107a7113",
+    "url": "/github.io/burger-constructor/static/css/main.e234ed16.chunk.css"
+  },
+  {
+    "revision": "44d02af8065c4b5e4140",
+    "url": "/github.io/burger-constructor/static/js/2.3be3ac4f.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/github.io/burger-constructor/static/js/2.3be3ac4f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a066cf9985e6107a7113",
+    "url": "/github.io/burger-constructor/static/js/main.623f21c0.chunk.js"
+  },
+  {
+    "revision": "5af804aa3de1add4103d",
+    "url": "/github.io/burger-constructor/static/js/runtime-main.d4574135.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/github.io/burger-constructor/static/media/logo.b8503d26.png"
+  }
+]);
